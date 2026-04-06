f1a0c098334ebb5cd2b70b38d9e86d6cd97ec785
Muneera Al Naabi <muneerasaifalnaabi@gmail.com>
2026-04-06 10:39:04 +0400

    solved task 2 :CommonEnd

--- src/firstLast6.java
+++ src/firstLast6.java
@@ -1,7 +1,12 @@
 public class firstLast6 {
     public static void main(String[] args) {
         int[] nums={1,2,6};
-        System.out.println(firstLast6(nums));
+       // System.out.println(firstLast6(nums));
+
+        //task 2
+        int[] a1 = {1, 2, 3};
+        int[] b1 = {7, 3};
+        System.out.println(commonEnd(a1, b1));
 
     }
 
@@ -19,4 +24,15 @@ public class firstLast6 {
 
 
     }
+    public static boolean  commonEnd(int[] a, int[] b){
+
+            if(a[0]==6 && b[0]==6|| (a[a.length - 1] == b[b.length - 1])) {
+                return true;
+            }
+
+
+        return false;
+
+    }
 }
+
