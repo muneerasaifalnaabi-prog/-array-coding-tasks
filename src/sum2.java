d786c912df6e4b7be39793a493844102a5c49efc
Muneera Al Naabi <muneerasaifalnaabi@gmail.com>
2026-04-06 15:27:59 +0400

    task 20 : array with sum of first 2 element

---
+++ src/sum2.java
@@ -0,0 +1,10 @@
+public class sum2 {
+    public static void main(String[] args) {
+        int[] arr = {1, 2, 3};
+        System.out.println(sum2(arr));
+
+    }
+    public static int sum2(int[] nums) {
+        return nums[0] + nums[1];
+    }
+}
